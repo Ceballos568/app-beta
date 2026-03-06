92466a281c439573a8e25f8ce697ae60fd986fb0
Sebas <sceballos@email.com>
2026-03-06 11:55:17 -0500

    creacion de database

---
+++ config/database.sql
@@ -0,0 +1,7 @@
+create table users(
+    Firstname  |  varchar(15)  |  not null
+    lastname   |  varchar(15)  |  not null
+    email      |  varchar(50)  |  not null
+    password   |  varchar(20)  |  not null
+    mobile_phone | varchar(20) |  not null
+);
