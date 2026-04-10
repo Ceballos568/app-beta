30b651b0ba5dc4a8e889185faf73fe7e69b9ab46
Sebas <sceballos@email.com>
2026-04-10 12:00:42 -0500

    list_user

---
+++ src/list_users.php
@@ -0,0 +1,42 @@
+<?php
+require("../config/database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border = "1" align ="center">
+        <tr>
+        <th>Fullname</th>
+        <th>E-mail</th>
+        <th>Mobile phone</th>
+        <th>Status</th>
+        <th>Photo</th>
+        <th>Options</th>
+        </tr>
+    <tr>
+        <td>Peter Loza</td>   
+        <td>Peter@mail.com</td>
+        <td>300123</td>
+        <td>Active</td>
+        <td><img src="profile_photos/user_default.png"
+            width="48" alt= "User photo"> 
+        </td>
+        <td>
+        <a href="#">
+            <img src="icons/icon.png"
+            with = "20" alt= "Edit user" >
+        </a>  
+        &nbsp;
+        <a href="#">
+            <img src="icons/delete.png" width = "20" alt="Delete user">
+        </a>
+    </td>
+</tr>
+</table>
+</body>
+</html>
--- src/signup.php
+++ src/signup.php
@@ -1,15 +1,54 @@
 <?php
-    include('../congif/database.php');
-    // get data
-    $f_name = $_Post ['fname'];
-    $l_name = $_Post ['lname'];
-    $e_mail = $_Post ['email'];
-    $m_phone = $_Post ['mphone'];
-    $p_sswd = $_Post ['passwd'];
-    $enc_pass= md5($p_sswd);
-    //Query to insert to SQL
-    $sql = "Insert  Into users (Fisrtnanem,lastname,email,mobilephone,password)
-            values ('$f_name','$l_name','$e_mail',$m_phone,'$p_sswd')";
+    include('../config/database.php');
+    
+    // Recibir datos
+    $f_name = $_POST['fname'];
+    $l_name = $_POST['lname'];
+    $e_mail = $_POST['email'];
+    $m_phone = $_POST['mphone'];
+    $p_sswd = $_POST['password'];
 
-    //Excute query
-    pg_query($sql);
+    // FEATURE 1: Validar email unico
+    $check_email = "SELECT email FROM users WHERE email = '$e_mail'";
+    $res_email = pg_query($conn, $check_email);
+
+    if (pg_num_rows($res_email) > 0) {
+        echo "Error: El correo electronico '$e_mail' ya esta registrado. Por favor, use uno diferente.";
+        exit();
+    }
+
+    // FEATURE 2: Validar telefono unico
+    $check_phone = "SELECT mobile_phone FROM users WHERE mobile_phone = '$m_phone'";
+    $res_phone = pg_query($conn, $check_phone);
+
+    if (pg_num_rows($res_phone) > 0) {
+        echo "Error: El numero de celular '$m_phone' ya esta registrado en nuestro sistema.";
+        exit();
+    }
+
+    // FEATURE 4: Encriptar contrasena
+    $enc_pass = password_hash($p_sswd, PASSWORD_BCRYPT);
+
+    // FEATURE 3: Insertar en ambas bases de datos
+    $sql = "INSERT INTO users (firstname, lastname, email, mobile_phone, psswd)
+            VALUES ('$f_name', '$l_name', '$e_mail', '$m_phone', '$enc_pass')";
+
+    // Guardar en local
+    $res_local = pg_query($conn, $sql);
+
+    if ($res_local) {
+        // Si funciono en local, guardar en Supabase
+        $res_supa = pg_query($supabase, $sql);
+
+        if ($res_supa) {
+            echo "Usuario registrado correctamente en ambas bases de datos";
+        } else {
+            echo "Error: Se guardo en local pero fallo en Supabase";
+        }
+    } else {
+        echo "Error: No se pudo registrar el usuario";
+    }
+
+    pg_close($conn);
+    pg_close($supabase);
+?>
